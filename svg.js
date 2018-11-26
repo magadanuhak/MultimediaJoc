8d95a0cc8dd0de859a5026d8cae99cc8150f0600
magadanuhak <magadanuhak@gmail.com>
2018-11-26 11:24:14 +0300

    Am adaugat js pentru svg

--- svg.js
+++ svg.js
@@ -1,6 +1,8 @@
-var ids = "";
+var ids = "M14635 M18374 M23852 M13728 M6827 M14635";
+ids = ids.split(' ');
 var jsoncontinente = {
     "continente": [
+        1,
         { "id": "M14635", "nume": "Europa", "fructe": [0, 3, 2] },
         { "id": "M18374", "nume": "Asia", "fructe": [0, 3, 2] },
         { "id": "M23852", "nume": "Australia", "fructe": [0, 3, 2] },
@@ -9,8 +11,17 @@ var jsoncontinente = {
         { "id": "M14635", "nume": "America de Sud", "fructe": [0, 3, 2] }
     ]
 };
-document.querySelector('svg').addEventListener('click', function (e) {
-    var el = e.target;
-    el;
-    console.log(el);
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector('svg').addEventListener('click', function (e) {
+        var el = e.target;
+        el;
+
+        if(jsoncontinente.continente.includes(1)){
+            console.log('este');
+        }else{
+            console.log('nu Este');
+        }
+    });
 });
+
